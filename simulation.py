7fa972c2642b66ee7635e30988ff182ae2b750f3
madrutis <madrutis@umich.edu>
2024-04-05 15:00:38 -0400

    Initial Commit

---
+++ simulation.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import IPython.display as display
+
+
+def main():
+
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("..")
